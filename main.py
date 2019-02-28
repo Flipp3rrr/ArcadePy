6db745f9f998200de625a5a3921aae12df48c98f
Flipp3rrr <philipkreuning@icloud.com>
2019-02-28 16:08:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Imports
+import tkinter as tk
+
+# Create root widget
+root = tk.Tk()
+
+# Create startup screen
+StartupScreen = tk.Label(root, text="ArcadePy")
+
+# Make startup screen the correct size
+StartupScreen.pack()
+
+# Show startup screen
+root.mainloop()
