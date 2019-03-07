c365c5f89b89b29d06d4389f338c4944d3f4c31b
Flipp3rrr <philipkreuning@icloud.com>
2019-03-07 10:20:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ base_folder = os.path.dirname(__file__)
 # Create root widget
 root = tk.Tk()
 
+# Pong script
+def PongCallback():
+	print("Pong is work in progress!")
+
 # Get logo directory
 logo_path = os.path.join(base_folder, "images/PythonLogoTiny.png")
 logo = tk.PhotoImage(file=logo_path)
@@ -17,6 +21,8 @@ w2 = tk.Label(root,
               justify=tk.LEFT,
               padx = 10, 
               text=description).pack(side="left")
+w3 = tk.Button(master, text = "Play Pong", command = PongCallback)
+w3.grid(row = 0, column = 0)
 root.mainloop()
 
 # Make startup screen the correct size
