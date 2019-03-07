2eb8186ffe2165fd1db8ab84fc41499fa83c6065
Flipp3rrr <philipkreuning@icloud.com>
2019-03-07 15:21:19 +0100

    Added "exit" button

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Imports
 import tkinter as tk
 import os
+import sys
 
 # Get script folder
 base_folder = os.path.dirname(__file__)
 
 # Create root widget
 root = tk.Tk()
+
+# Exit script
+def ExitCallback():
+	sys.exit("Thanks for playing!")
 
 # Pong script
 def PongCallback():
@@ -15,10 +20,11 @@ def PongCallback():
 # Get logo directory
 logo_path = os.path.join(base_folder, "images/PythonLogoTiny.png")
 logo = tk.PhotoImage(file=logo_path)
-w1 = tk.Label(root, image=logo).pack(side="left")
-description = """ArcadePy is a small suite of arcade games written in Python."""
-w2 = tk.Label(root, justify=tk.LEFT, padx = 10, text=description).pack(side="left")
-w3 = tk.Button(root, justify=tk.CENTER, text = "Play Pong", command = PongCallback).pack(side="bottom")
+PythonLogoWidget = tk.Label(root, image=logo).pack(side="left")
+DescriptionText = """ArcadePy is a small suite of arcade games written in Python."""
+DescriptionTextWidget = tk.Label(root, justify=tk.LEFT, padx = 10, text=DescriptionText).pack(side="left")
+PongButtonWidget = tk.Button(root, justify=tk.CENTER, text="Play Pong", command=PongCallback).pack(side="bottom")
+ExitButtonWidget = tk.Button(root, justify=tk.CENTER, text="Exit", command=ExitCallback).pack(side="bottom")
 root.mainloop()
 
 # Make startup screen the correct size
