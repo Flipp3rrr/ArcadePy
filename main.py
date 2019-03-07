b42f8486290bc1b79ff3ece510f2d62fef8d2829
Flipp3rrr <philipkreuning@icloud.com>
2019-03-07 13:59:22 +0100

    Added some stuff for the first game: Pong

--- main.py
+++ main.py
@@ -17,12 +17,8 @@ logo_path = os.path.join(base_folder, "images/PythonLogoTiny.png")
 logo = tk.PhotoImage(file=logo_path)
 w1 = tk.Label(root, image=logo).pack(side="left")
 description = """ArcadePy is a small suite of arcade games written in Python."""
-w2 = tk.Label(root, 
-              justify=tk.LEFT,
-              padx = 10, 
-              text=description).pack(side="left")
-w3 = tk.Button(master, text = "Play Pong", command = PongCallback)
-w3.grid(row = 0, column = 0)
+w2 = tk.Label(root, justify=tk.LEFT, padx = 10, text=description).pack(side="left")
+w3 = tk.Button(root, justify=tk.CENTER, text = "Play Pong", command = PongCallback).pack(side="bottom")
 root.mainloop()
 
 # Make startup screen the correct size
