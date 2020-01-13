3896b103ad07ef3cf98057d9645f13f72f3e592b
Flipp3rrr <philipkreuning@icloud.com>
2020-01-13 14:30:30 +0100

    Improve error handling on startup from 52 to 27

--- games/pong.py
+++ games/pong.py
@@ -6,46 +6,22 @@
 
 sysWorks = True
 osWorks = True
+
 try:
 	import sys
 except:
 	sysWorks = False
-try:
-	import tkinter as tk
-except:
-	if sysWorks == True:
-		sys.exit("Error: Couldn't import the tkinter module, is it installed?")
-	elif osWorks == True:
-		os._exit()
-	else:
-		try:
-			exit("Error: Multiple modules couldn't be loaded!")
-		except:
-			quit()
+	exit("Fatal error: The SYS module couldn't be imported, this could be happening because Python isn't properly installed.")
+
 try:
 	import os
 except:
 	osWorks = False
-	if sysWorks == True:
-		sys.exit("Error: Couldn't import the os module, is it installed?")
-	elif osWorks == True:
-		os._exit()
-	else:
-		try:
-			exit("Error: Multiple modules couldn't be loaded!")
-		except:
-			quit()
+	exit("Fatal error: The OS module couldn't be imported, this could be happening because Python isn't properly installed.")
+
 try:
+	import tkinter as tk
 	import random
 	import time
 except:
-	if sysWorks
-
-if sysWorks == False:
-	if osWorks == True:
-		os._exit("Error: The SYS isn't imported, this could be happening because Python isn't properly installed.")
-	elif osWorks == False:
-		try:
-			exit("Error: The SYS and OS modules aren't imported, this could be happening because Python isn't properly installed.")
-		except:
-			quit()
+	sys.exit("Fatal error: One or more default imports failed, this could be happening because Python isn't properly installed.")
