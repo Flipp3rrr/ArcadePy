3eaa97d5a30d7b22831fa41b544a7448d3b6b062
Flipp3rrr <flipp3rrr@protonmail.com>
2020-11-07 17:03:54 +0100

    Finished pong!
    Please note that the "Quit" button that appears when you lose doesn't work yet.

--- games/pong.py
+++ games/pong.py
@@ -4,19 +4,10 @@
 # All code is licensed under the UNLICENSE
 #
 
-sysWorks = True
-osWorks = True
-
-try:
-	import sys
-except:
-	sysWorks = False
-	exit("Fatal error: The SYS module couldn't be imported, this could be happening because Python isn't properly installed.")
-
+# Imports
 try:
 	import os
 except:
-	osWorks = False
 	exit("Fatal error: The OS module couldn't be imported, this could be happening because Python isn't properly installed.")
 
 try:
@@ -24,6 +15,111 @@ try:
 	import random
 	import time
 except:
-	sys.exit("Fatal error: One or more imports failed, this could be happening because Python isn't properly installed.")
+	exit("Fatal error: One or more imports failed, this could be happening because Python isn't properly installed.")
 
-print("Success!")
+# Keep score
+score = 0
+
+# Quit game
+def quitGame():
+	exit("Game was quit!")
+
+# Ball class
+class Ball:
+	def __init__(self, canvas, color, paddle):
+		self.canvas = canvas
+		self.paddle = paddle
+		self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+		self.canvas.move(self.id, 245, 100)
+		starts = [-3, -2, -1, 1, 2, 3]
+		random.shuffle(starts)
+		self.x = starts[0]
+		self.y = -3
+		self.canvas_height = self.canvas.winfo_height()
+		self.canvas_width = self.canvas.winfo_width()
+		self.hit_bottom = False
+	
+	# Paddle detection
+	def hit_paddle(self, pos):
+		paddle_pos = self.canvas.coords(self.paddle.id)
+		if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+			if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+				return True
+			return False
+
+	def draw(self):
+		global score
+		self.canvas.move(self.id, self.x, self.y)
+		pos = self.canvas.coords(self.id)
+		if pos[1] <= 0:
+			self.y = 3
+
+		# Detect hit bottom
+		if pos[3] >= self.canvas_height:
+			self.hit_bottom = True
+		
+		# Detect hit paddle
+		if self.hit_paddle(pos) == True:
+			self.y = -3
+			score = score + 1
+			return score
+
+		if pos[0] <= 0:
+			self.x = 3
+		if pos[2] >= self.canvas_width:
+			self.x = -3
+
+# Paddle class
+class Paddle:
+	def __init__(self, canvas, color):
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+		self.canvas.move(self.id, 200, 300)
+		self.x = 0
+		self.canvas_width = self.canvas.winfo_width()
+		self.canvas.bind_all("<KeyPress-Left>", self.go_left)
+		self.canvas.bind_all("<KeyPress-Right>", self.go_right)
+	
+	def go_left(self, evt):
+		self.x = -2.5
+	
+	def go_right(self, evt):
+		self.x = 2.5
+
+	def  draw(self):
+		self.canvas.move(self.id, self.x, 0)
+		pos = self.canvas.coords(self.id)
+		if pos[0] <= 0:
+			self.x = 0
+		if pos[2] >= self.canvas_width:
+			self.x = 0
+
+# Make life easier
+root = tk.Tk()
+
+# Ready the playing field
+root.title("Pong")
+root.resizable(0, 0)
+root.wm_attributes("-topmost", 1)
+canvas = tk.Canvas(root, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+root.update()
+
+# Ready the game over screen
+gameOverButton = tk.Button(root, text="Quit", command=quitGame)
+
+# Spawn paddle and ball
+paddle = Paddle(canvas, "blue")
+ball = Ball(canvas, "red", paddle)
+
+# Constant updates
+while 1:
+	if ball.hit_bottom == False:
+		ball.draw()
+		paddle.draw()
+	else:
+		gameOverButton.pack()
+		pass
+	root.update_idletasks()
+	root.update()
+	time.sleep(0.01)
